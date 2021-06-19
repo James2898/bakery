5c6131964327de7d520ba6a3544f9e76cb13e396
James Martinez <amajamesmartinez@gmail.com>
2021-06-19 21:12:04 +0800

    About Modifications

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,9 +30,10 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            // 'name'      => 'required|string|max:255',
-            // 'email'     => 'required|string|email|max:255|unique:users',
-            // 'user_address'   => 'required|string|max:255',
+            'user_name'      => 'required|string|max:255',
+            'user_email'     => 'required|string|email|max:255|unique:users',
+            'user_address'   => 'required|string|max:255',
+            'user_mobile'    => 'required|digits:11'
             // 'user_password'  => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -56,10 +57,11 @@ class UserController extends Controller
     public function update(Request $request)
     {
         $request->validate([
-            // 'name' => 'required',
-            // 'introduction' => 'required',
-            // 'location' => 'required',
-            // 'cost' => 'required'
+            'user_name'      => 'required|string|max:255',
+            // 'user_email'     => 'required|string|email|max:255|unique:users',
+            // 'user_address'   => 'required|string|max:255',
+            'user_mobile'    => 'required|digits:11'
+            // 'user_password'  => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         User::find($request->user_no)->update([
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,21 +2,35 @@
     <div class="pt-24">
         <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
         <!--Left Col-->
-            <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-                <h1 class="my-4 text-5xl font-bold leading-tight">
-                About
-                </h1>
-            <p class="leading-normal text-2xl mb-8">
-                Give the gift of good bread and pastries to the special persons in your life. Click here to find our premium breads!
-            </p>
-            <a class="btn mx-auto lg:mx-0 hover:underline bg-yellow-500 text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-            Shop Now
-            </a>
-            </div>
+        <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+            <h1 class="my-4 text-5xl font-bold leading-tight">
+                Our Concept & Philosophy
+            </h1>
+        <p class="leading-normal text-2xl mb-8">
+            LMJZ Bakery breads are made fresh from scratch using the highest quality ingredients. In addition to our pandesal, we also make soft-crusted sandwich breads, specialty breads and other baked treats like muffins and ensaymadas. We encourage a more healthy and natural lifestyle, having it as one of our company values.
+        </p>
+        </div>
         <!--Right Col-->
         <div class="w-full md:w-3/5 py-6 text-center">
-            <img class="w-full md:w-4/5 z-50" src="{{ asset('img/bread_homepage.png') }}" />
+            <img class="w-full md:w-4/5 z-50" src="{{ asset('img/bread_about1.png') }}" />
+        </div>
         </div>
     </div>
-</div>
+    <div class="pt-24">
+        <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <!--Left Col-->
+        <div class="w-full md:w-3/5 py-6 text-center">
+            <img class="w-full md:w-4/5 z-50" src="{{ asset('img/bread_about2.png') }}" />
+        </div>
+        <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+            <h1 class="my-4 text-5xl font-bold leading-tight">
+                Our Product Line-Up
+            </h1>
+            <!--Right Col-->
+            <p class="leading-normal text-2xl mb-8">
+                We have a wide range of breads, from the most basic all-time favorite Filipino Pandesal, to other flavored and speciality breads. Click <a href="{{route('products')}}" class="font-bold">here</a> to view our menu.
+            </p>
+        </div>
+        </div>
+    </div>
 </x-app-layout>
